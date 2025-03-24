24d1bc489bbc70d7bcbfff9e8da2e4137bed88e0
luism2302 <luis.urzua2302@gmail.com>
2025-03-24 13:55:25 -0600

    Started working on the squares, will continue on building the entire board

--- board.py
+++ board.py
@@ -1,1 +1,21 @@
 import pygame
+
+class Square:
+	def __init__(self, color, left = 10, top = 10, width = 50, height = 50):
+		self._left = left
+		self._top = top
+		self._width = width
+		self._height = height
+		self._rectangle = pygame.Rect(self._left, self._top, self._width, self._height)
+
+		match color:
+			case "black":
+				self._color = pygame.Color(0,0,0)
+			case "white":
+				self._color = pygame.Color(255,255,255)
+			case _:
+				raise(Exception("Invalid color: only black and white allowed"))
+	
+	def _draw_square(self, surface):
+		pygame.draw.rect(surface, self._color, self._rectangle)
+	
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import pygame, sys
+from board import Square
 def main():
-	print("We good")
+	pygame.init()
+	screen = pygame.display.set_mode((800,800))
+	background = (0,183,235)
+	screen.fill(background)
+	pygame.display.flip()
 
+	test_square_black = Square("black")
+	test_square_white = Square("white")
+
+	test_square_white._draw_square(screen)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+
+		pygame.display.flip()
+	
 
 if __name__ == "__main__":
 	main()
